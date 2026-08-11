fe95933e7247e73f440c03abb5a55934f0759a64
Stalin-dev <stalinlopezm@gmail.com>
2026-08-11 01:30:58 -0500

    Conecction mongo and Clerk

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,65 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
+
+export const inngest = new Inngest({ id: "Ecommerce" });
+
+export const syncUser = inngest.createFunction(
+    {
+        id: "sync-user",
+        triggers: {
+            event: "clerk/user.created"
+        }
+    },
+
+    async ({ event }) => {
+        await connectDB();
+
+        const {
+            id,
+            email_addresses,
+            first_name,
+            last_name,
+            image_url
+        } = event.data;
+
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name || ""} ${last_name || ""}`.trim(),
+            imageurl: image_url,
+            address: [],
+            wishlist: []
+        };
+
+        console.log("Nuevo usuario:", newUser);
+
+        return newUser;
+    }
+);
+
+const deleteUserFromDB = inngest.createFunction(
+    {
+        id: "delete-user",
+        triggers: {
+            event: "clerk/user.deleted"
+        }
+    },
+    async ({ event }) => {
+        await connectDB();
+
+        const { id } = event.data;
+
+        await User.deleteOne({
+            clerkId: id
+        });
+
+        console.log(`Usuario eliminado: ${id}`);
+
+        return {
+            success: true
+        };
+    }
+);
+
+export const functions = [syncUser,deleteUserFromDB];
