710b701dfdc378f7ede5668af15f991998604847
Stalin-dev <stalinlopezm@gmail.com>
2026-08-11 12:00:33 -0500

    Inngest new method

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -3,23 +3,60 @@ import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
 
 export const inngest = new Inngest({
-    id: "ecommerce-app"
+    id: "ecommerce-app",
+    signingKey: process.env.INNGEST_SIGNING_KEY
 });
 
 const syncUser = inngest.createFunction(
-    { id: "sync-user",triggers: [{event: "clerk/user.created"}] },
+    {
+        id: "sync-user",
+        triggers: [
+            { event: "clerk/user.created" }
+        ]
+    },
     async ({ event }) => {
-        console.log("Syncing user to DB", event.data);
+        await connectDB();
+
+        const {
+            id,
+            email_addresses,
+            first_name,
+            last_name,
+            image_url
+        } = event.data;
+
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`.trim() || "User",
+            imageUrl: image_url,
+            addresses: [],
+            wishlist: []
+        };
+
+        await User.create(newUser);
+        console.log(`✅ Usuario creado: ${id}`);
     }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id: "delete-user-from-db",triggers: [{event: "clerk/user.deleted"}]},
+    {
+        id: "delete-user-from-db",
+        triggers: [
+            { event: "clerk/user.deleted" }
+        ]
+    },
     async ({ event }) => {
-        console.log("Deleting user from DB", event.data);
+        await connectDB();
+
+        const { id } = event.data;
+
+        await User.deleteOne({
+            clerkId: id
+        });
+        console.log(`✅ Usuario eliminado: ${id}`);
     }
 );
-    
 
 export const functions = [
     syncUser,
